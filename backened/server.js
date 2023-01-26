c80469406d16a7db0152bef79e141ab687fd3573
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2023-01-26 17:58:55 +0530

    Add files via upload

--- backened/server.js
+++ backened/server.js
@@ -1,1 +1,16 @@
+const express = require('express')
 
+const app = express()
+app.get((req, res) => {
+    res.send("hello")
+})
+
+const PORT = 2000
+app.listen(PORT, (req, res) => {
+    try {
+        console.log("")
+    } catch (err) {
+        console.log("")
+    }
+    console.log("listining on port 2000")
+})
